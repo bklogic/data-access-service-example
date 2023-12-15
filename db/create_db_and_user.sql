c4f49b7aff90eb84e1bbeca9d8c79a16982362a6
ken <ken@backlogic.net>
2023-12-15 09:01:27 -0800

    update db scripts

--- db/create_db_and_user.sql
+++ db/create_db_and_user.sql
@@ -2,14 +2,12 @@
 DROP DATABASE IF EXISTS `basic`; 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`basic` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-# create database classicmodel
+# create database classicmodels
 DROP DATABASE IF EXISTS `classicmodels`; 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`classicmodels` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-# create user
-# create user "prst"
-DROP USER IF EXISTS 'prst'@'%';
-CREATE USER 'prst'@'%' IDENTIFIED BY 'prst';
-GRANT ALL PRIVILEGES ON basic.* TO 'prst'@'%';
-GRANT ALL PRIVILEGES ON classicmodels.* TO 'prst'@'%';
-
+# create user "backlogic"
+DROP USER IF EXISTS 'backlogic'@'%';
+CREATE USER 'backlogic'@'%' IDENTIFIED BY 'backlogic';
+GRANT ALL PRIVILEGES ON basic.* TO 'backlogic'@'%';
+GRANT ALL PRIVILEGES ON classicmodels.* TO 'backlogic'@'%';
